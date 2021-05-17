1da7a85cf3011789d707f168ee787bfc673b1676
byronotravez <byron@esotroejemplo.es>
2021-05-17 19:21:41 +0200

    uso de metodos

--- actividad_4/src/actividad_4/Principal.java
+++ actividad_4/src/actividad_4/Principal.java
@@ -7,6 +7,7 @@ public class Principal {
 	public static void main(String[] args) {
 		System.out.println("creacion de variables del ejercicio.");
 		System.out.println("Creacion de metodos");
+		System.out.println("resolucion del ejercicio.");
 		//--------------------------------------------------------
 		
 		
@@ -14,7 +15,13 @@ public class Principal {
 
 		Scanner teclado = new Scanner(System.in);
 		int resultado, valor1, valor2;
+		valor1 = datos(teclado, "Introduce el primer valor: ");
+		valor2 = datos(teclado, "Introduce el segundo valor: ");
 		
+		resultado = suma(valor1, valor2);
+		
+		System.out.println("La suma de los valores es: " + resultado);
+
 
 	}
 	
