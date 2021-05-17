5d653b4039f45da2c1aa5cca3cbdad7307327032
byronotravez <byron@esotroejemplo.es>
2021-05-17 19:13:42 +0200

    este primer commit sirve para crear las variables necesarias y paquetes.

---
+++ actividad_4/src/actividad_4/Principal.java
@@ -0,0 +1,17 @@
+package actividad_4;
+
+import java.util.Scanner;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola, Byron");
+
+		Scanner teclado = new Scanner(System.in);
+		int resultado, valor1, valor2;
+		
+		System.out.println("creacion de variables del ejercicio.");
+
+	}
+}
